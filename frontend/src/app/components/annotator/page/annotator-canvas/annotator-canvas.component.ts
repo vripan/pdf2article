770ea4327359436a379335852eb21c7f8d25af7c
Buturuga Alexandru <buturuga.alexandru.iulian@gmail.com>
2018-12-05 19:33:44 +0200

    [annotator] small page component update

--- frontend/src/app/components/annotator/page/annotator-canvas/annotator-canvas.component.ts
+++ frontend/src/app/components/annotator/page/annotator-canvas/annotator-canvas.component.ts
@@ -1,18 +1,33 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, AfterViewInit, ElementRef, Input } from '@angular/core';
 
 @Component({
   selector: 'annotator-canvas',
   templateUrl: './annotator-canvas.component.html',
   styleUrls: ['./annotator-canvas.component.scss']
 })
-export class AnnotatorCanvasComponent implements OnInit {
+export class AnnotatorCanvasComponent implements AfterViewInit {
 
   @Input()
   public viewport: any;
 
-  constructor() { }
+  constructor(
+    private element: ElementRef
+  ) { }
 
-  ngOnInit() {
+  public ngAfterViewInit(): void {
+    const $canvas = this.createAnnotationCanvas(this.viewport);
+    this.element.nativeElement.appendChild($canvas);
+  }
+
+  private createAnnotationCanvas(viewport): HTMLCanvasElement {
+    const { width, height } = viewport,
+          $canvas = document.createElement('canvas');
+
+    $canvas.classList.add('page-canvas');
+    $canvas.width = width;
+    $canvas.height = height;
+
+    return $canvas;
   }
 
 }
