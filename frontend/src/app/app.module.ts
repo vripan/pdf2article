896023e1bc5da236576bfb8d487b553badccde5a
Ceicoschi Gabriel <gabriel@sprybase.com>
2018-12-05 19:46:05 +0200

    landing ui

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,12 +6,18 @@ import { AppComponent } from './app.component';
 
 import { HomeComponent } from './components/home/home.component';
 import { Error404Component } from './components/shared/error404/error404.component';
+import { HeaderComponent } from './components/header/header.component';
+import { UploadMainComponent } from './components/upload-main/upload-main.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    Error404Component
+    Error404Component,
+    HeaderComponent,
+    UploadMainComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
