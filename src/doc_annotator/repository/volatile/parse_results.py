1a63993b4911d41891c97fd240c064878cabfa4d
vladimiripan <vladimilie@gmail.com>
2018-12-06 15:02:39 +0200

    Database structure

---
+++ src/doc_annotator/repository/volatile/parse_results.py
@@ -0,0 +1,19 @@
+# implementarea se va face cu ajutorul unui dictionar, unde cheia este data de hash
+# operatiile trebuie sa fie thread-safe
+# se returneaza o copie a rezultatlui din dictionar, nu o referinta la acesta (vezi deep_clone, clone, copy)
+
+
+def get_results(hash):
+    """
+    Returns results for specified hash, or None if hash entry is invalid
+    """
+    pass
+
+
+def store_results(hash, results):
+    """
+    Saves results for specified hash.
+    If there is already an entry with requested hash,
+    old entry will be replaced by the new one
+    """
+    pass
---
+++ src/doc_annotator/repository/volatile/training_metadata.py
@@ -0,0 +1,19 @@
+# la fel ca la parse_results
+# la inchiderea serverlui datele din acest bd trebuie salvate pe disk, folosind orice metoda de serializare
+# la pornirea serverlui dataele trebuie citiet de pe disc, deserializate si aduse in memorie
+
+
+def get_metadata(hash):
+    """
+    Returns metadata for specified hash, or None if hash entry is invalid
+    """
+    pass
+
+
+def store_metadata(hash, metadata):
+    """
+    Saves metadata for specified hash.
+    If there is already an entry with requested hash,
+    old entry will be replaced by the new one
+    """
+    pass
