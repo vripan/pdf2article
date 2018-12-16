b6b5198756bd66eddc203e0ed546d31fcc6dcb71
vladimiripan <vladimilie@gmail.com>
2018-12-16 18:23:11 +0200

    PageSeg

---
+++ src/doc_annotator/utils/page_segmentation/page_segmentation.py
@@ -0,0 +1,7 @@
+def get_page_as_image(file_hash, page_num):
+    #dechide pdf-ul din pdf_files cu numele file_hash
+    #returneaza pagina cu numarul page_num din fisierul deschis anterior
+    pass
+
+
+
