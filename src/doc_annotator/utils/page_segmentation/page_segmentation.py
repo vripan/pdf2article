c27e9c61822391639383b31a8e3a2700bbfff10e
BogdanConstantin <constantin.bogdan.mirza@gmail.com>
2018-12-16 19:46:27 +0200

    Added function for getting page from a book

--- src/doc_annotator/utils/page_segmentation/page_segmentation.py
+++ src/doc_annotator/utils/page_segmentation/page_segmentation.py
@@ -1,7 +1,11 @@
+import pdf2image
+import os
+from doc_annotator import app
+import numpy
+
 def get_page_as_image(file_hash, page_num):
-    #dechide pdf-ul din pdf_files cu numele file_hash
-    #returneaza pagina cu numarul page_num din fisierul deschis anterior
-    pass
+    file_path = os.path.join(app.config['UPLOAD_FOLDER'], file_hash)
+    return numpy.array(pdf2image.convert_from_path(file_path, first_page=page_num, last_page=page_num)[0])
 
 
 
