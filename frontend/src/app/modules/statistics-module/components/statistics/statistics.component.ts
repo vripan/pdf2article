5590fd532c40531d7d9b67544a5f248df4f67baf
androbianca <biaanca.9805@yahoo.com>
2018-12-19 17:55:35 +0200

    statistics - partially updated

--- frontend/src/app/modules/statistics-module/components/statistics/statistics.component.ts
+++ frontend/src/app/modules/statistics-module/components/statistics/statistics.component.ts
@@ -1,18 +1,30 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit, ViewChildren } from '@angular/core';
 import { Chart } from 'chart.js';
+
+
+export interface DonutChartData {
+  name: string;
+  datasets: [{
+    data: number[];
+    backgroundColor: string[];
+  }];
+  labels: string[];
+}
 
 @Component({
   selector: 'app-statistics',
   templateUrl: './statistics.component.html',
   styleUrls: ['./statistics.component.scss']
 })
-export class StatisticsComponent implements OnInit {
+export class StatisticsComponent implements AfterViewInit {
 
-  @ViewChild('donutChart') private donutChart;
+ // @ViewChildren('accuracyChart somethingChart somethingelseChart') private donutCharts;
+  @ViewChildren('accuracyChart') private donutCharts;
+  @ViewChild('barChart') private barChart;
 
   // Table data
   public displayedColumns: string[] = ['position', 'location', 'views', 'files', 'conversion', 'total'];
-  public dataSource: Array<any> = [
+  public dataSource: any[] = [
     { position: 1, location: 'file1.pdf', views: 1.0079, files: 4.4325, conversion: '10%', total: 43.3425 },
     { position: 1, location: 'file1.pdf', views: 1.0079, files: 4.4325, conversion: '10%', total: 43.3425 },
     { position: 1, location: 'file1.pdf', views: 1.0079, files: 4.4325, conversion: '10%', total: 43.3425 },
@@ -24,29 +36,60 @@ export class StatisticsComponent implements OnInit {
     { position: 1, location: 'file1.pdf', views: 1.0079, files: 4.4325, conversion: '10%', total: 43.3425 },
   ];
 
-  // Donut charts data
-  private data = {
+  public accuracyChartData: DonutChartData = {
+    name: 'accuracyChartData',
     datasets: [{
-      data: [10, 20, 30]
-    }],
-
-    // These labels appear in the legend and in the tooltips when hovering different arcs
-    labels: [
-      'Red',
-      'Yellow',
-      'Blue'
-    ]
+      data: [122, 28, 33, 49, 53],
+      backgroundColor: ['rgb(255, 99, 132)', 'rgb(54, 162, 235)', 'rgb(255, 205, 86)', 'rgb(54, 162, 235)', 'rgb(255, 205, 86)']
+    }], labels: ['PDF1', 'PDF2', 'PDF3', 'PDF4']
   };
 
-  public ngOnInit(): void {
-    this.populateChart();
+  public somethingChartData: DonutChartData = {
+    name: 'somethingChartData',
+    datasets: [{
+      data: [22, 44, 3, 45, 5],
+      backgroundColor: ['rgb(255, 99, 132)', 'rgb(54, 162, 235)', 'rgb(255, 205, 86)', 'rgb(54, 162, 235)', 'rgb(255, 205, 86)']
+    }], labels: ['PDF1', 'PDF2', 'PDF3', 'PDF4']
+  };
+
+  public somethingelseChartData: DonutChartData = {
+    name: 'somethingelseChartData',
+    datasets: [{
+      data: [1, 2, 33, 4, 55],
+      backgroundColor: ['rgb(255, 99, 132)', 'rgb(54, 162, 235)', 'rgb(255, 205, 86)', 'rgb(54, 162, 235)', 'rgb(255, 205, 86)']
+    }], labels: ['PDF1', 'PDF2', 'PDF3', 'PDF4']
+  };
+
+  public barChartData: DonutChartData = {
+    name: 'barChartData',
+    datasets: [{
+      data: [66, 84, 83, 45, 95],
+      backgroundColor: ['rgb(255, 99, 132)', 'rgb(54, 162, 235)', 'rgb(255, 205, 86)', 'rgb(54, 162, 235)', 'rgb(255, 205, 86)']
+    }], labels: ['PDF1', 'PDF2', 'PDF3', 'PDF4']
+  };
+
+  private globalDataSource: DonutChartData[] = [
+    this.accuracyChartData,
+    this.somethingChartData, this.somethingelseChartData
+  ];
+
+  public ngAfterViewInit(): void {
+    this.populateDonutCharts();
+    this.populateChart(this.barChart, 'bar');
   }
 
-  private populateChart(): void {
-    const donutChart1 = new Chart(this.donutChart, {
-      type: 'doughnut',
-      data: this.data,
-      // options: options
+  private populateChart(element, type) {
+    const dataId = element.nativeElement.getAttribute('data-id');
+    const chart = new Chart(element.nativeElement, {
+      type: type,
+      data: this.globalDataSource.find(x => x.name === dataId),
     });
+  }
+
+  private populateDonutCharts(): void {
+    this.donutCharts.forEach(element => {
+      this.populateChart(element, 'doughnut');
+    });
+
   }
 }
