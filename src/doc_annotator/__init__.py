32752eed5d5f70da18b6f9036fb0c16859fe29c5
Vladimir RIPAN <vladimilie@gmail.com>
2019-01-10 22:03:02 +0200

    Added aggressive python version check

--- src/doc_annotator/__init__.py
+++ src/doc_annotator/__init__.py
@@ -1,5 +1,9 @@
-from flask import Flask
 import os, sys
+
+if not sys.version_info == (3, 6, 4, 'final', 0) or sys.version.find('64 bit (AMD64)') == -1:
+    raise Exception("Wrong python version, your are using %s, required 3.6.4 x64. Download from here https://www.python.org/ftp/python/3.6.4/python-3.6.4-amd64.exe" % sys.version)
+
+from flask import Flask
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -31,7 +35,7 @@ import doc_annotator.repository.volatile.parse_results
 import doc_annotator.repository.volatile.training_metadata
 
 if not os.path.exists(app.config['UPLOAD_FOLDER']) or not os.path.exists(app.config['TRAINING_FOLDER']):
-   raise Exception("Check if you have folder 'pdf_files' and 'training_data")
+    raise Exception("Check if you have folder 'pdf_files' and 'training_data")
 
 if not os.path.abspath(os.curdir).split(os.sep)[-1].startswith('pdf2article'):
-    raise Exception("Invalid current working directory! Your current working directory is '%s' and should be the pdf2article folder." % os.path.abspath(os.curdir))
+    raise Exception("Invalid current working directory! Your current working directory is '%s' and should be the pdf2article folder.\nIf you are in the right folder then rename the folder to begin with pdf2article" % os.path.abspath(os.curdir))
