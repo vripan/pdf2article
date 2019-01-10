92116c4dc8bd8ed54a787f9d12e7c04c435c98e6
Vladimir RIPAN <vladimilie@gmail.com>
2019-01-10 19:52:14 +0200

    Added setup

---
+++ setup.bat
@@ -0,0 +1,6 @@
+pip install -r requirements.txt
+setx FLASK_APP doc_annotator
+setx FLASK_ENV development
+pip install -e ./src
+
+pause
--- src/doc_annotator/__init__.py
+++ src/doc_annotator/__init__.py
@@ -30,7 +30,8 @@ import doc_annotator.controllers.training
 import doc_annotator.repository.volatile.parse_results
 import doc_annotator.repository.volatile.training_metadata
 
-if not os.path.exists(app.config['UPLOAD_FOLDER']) or not os.path.exists(app.config['TRAINING_FOLDER']) \
-        or not os.path.abspath(os.curdir).split(os.sep)[-1] == 'pdf2article':
-    raise Exception("Invalid current working directory! Your current working directory is"
-                    " '%s' and should be the pdf2article folder." % os.curdir)
+if not os.path.exists(app.config['UPLOAD_FOLDER']) or not os.path.exists(app.config['TRAINING_FOLDER']):
+   raise Exception("Check if you have folder 'pdf_files' and 'training_data")
+
+if not os.path.abspath(os.curdir).split(os.sep)[-1].startswith('pdf2article'):
+    raise Exception("Invalid current working directory! Your current working directory is '%s' and should be the pdf2article folder." % os.path.abspath(os.curdir))
