6095cf52d11179671ea10a379233613642697fab
Lucian Cumpata <cumpata.lu@outlook.com>
2019-01-13 15:51:16 +0200

    Update optical.py
    Added test_get_number_of_upper_words

--- src/doc_annotator/unit_testing/utils/ocr/optical.py
+++ src/doc_annotator/unit_testing/utils/ocr/optical.py
@@ -4,7 +4,7 @@ from doc_annotator.utils.ocr.optical import get_number_of_words
 from doc_annotator.utils.ocr.optical import get_text_size
 from doc_annotator.utils.ocr.optical import get_string_without_punc
 from doc_annotator.utils.ocr.optical import get_number_of_capitalized_words
-
+from doc_annotator.utils.ocr.optical import get_number_of_upper_words
 
 class TestOptical(unittest.TestCase):
 
@@ -23,4 +23,9 @@ class TestOptical(unittest.TestCase):
     def test_get_number_of_capitalized_words(self):
         results = get_number_of_capitalized_words("Word1 Word2", 2)
         self.assertEqual(results, 100)
+
+    def test_get_number_of_upper_words(self):
+        results = get_number_of_upper_words("HELLO to UPPER WORLD!",4)
+        self.assertEqual(results, 75)
+
 
