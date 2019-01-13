cb9686a51f39739b0ce1c34514ca0423f6a70edb
adriansonea <ady_sonea@yahoo.com>
2019-01-13 14:52:59 +0200

    Update optical.py

--- src/doc_annotator/unit_testing/utils/ocr/optical.py
+++ src/doc_annotator/unit_testing/utils/ocr/optical.py
@@ -2,6 +2,8 @@ import unittest
 
 from doc_annotator.utils.ocr.optical import get_number_of_words
 from doc_annotator.utils.ocr.optical import get_text_size
+from doc_annotator.utils.ocr.optical import test_get_string_without_punc
+from doc_annotator.utils.ocr.optical import get_number_of_capitalized_words
 
 
 class TestOptical(unittest.TestCase):
@@ -13,3 +15,12 @@ class TestOptical(unittest.TestCase):
     def test_get_text_size(self):
         results = get_text_size("test1. test2.")
         self.assertEqual(results, 2)
+
+    def test_get_string_without_punc(self):
+        results = get_string_without_punc("Ana, are... mere;.?")
+        self.assertEqual(results, "Ana are mere")
+
+    def test_get_number_of_capitalized_words(self):
+        results = get_number_of_capitalized_words("Word1 Word2", 2)
+        self.assertEqual(results, 100)
+
